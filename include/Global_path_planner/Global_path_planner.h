118c7fc08e2dc5e7357c854d1a584967afc5c342
MarikaTakasago <marikanboumarch@yahoo.co.jp>
2021-03-24 14:04:08 +0900

    second change add thick_wall

--- include/Global_path_planner/Global_path_planner.h
+++ include/Global_path_planner/Global_path_planner.h
@@ -15,6 +15,13 @@ struct open
     int y;
 };
 
+struct twod
+
+{
+    int x;
+    int y;
+};
+
 
 
 class AStarPath
@@ -28,7 +35,7 @@ class AStarPath
         void map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
         void thick_wall(); //壁を増やして本当の壁にぶつからないようにする。
         void set_goal(); //goalを決める
-        void make_heuristic(); //heuristic関数をつくる
+        void make_heuristic(int,int); //heuristic関数をつくる
         void A_star(); //A*プロセス
 
 
@@ -38,16 +45,26 @@ class AStarPath
         int row;
         int col;
         std::vector<std::vector<int>> map_grid; //2Dmap
+        std::vector<std::vector<int>> map_grid_copy; //2Dmapcopy
 
         std::vector<std::vector<int>> heuristic; //heuristic関数
+        int dx;
+        int dy;
+        std::vector<twod> goal; //set_goal
+
+        std::vector<std::vector<int>> closed;
+        std::vector<std::vector<int>> open;
 
         bool map_checker;//map貰ったかどうか✓
+        bool wall_checker;//壁増やした？
+        bool reached;//goalについたか
 
 
         //member
         ros::NodeHandle nh;
         ros::NodeHandle private_nh;
         ros::Subscriber sub_map;
+        ros::Publisher pub_map;
         ros::Publisher pub_path;
         nav_msgs::OccupancyGrid the_map;//map格納
         nav_msgs::Path global_path;//Local_path_plannerにおくる
--- src/Global_path_planner.cpp
+++ src/Global_path_planner.cpp
@@ -5,12 +5,15 @@ AStarPath::AStarPath():private_nh("")
     //paramきめたらそれ書く
     private_nh.param("hz",hz,{10});
     private_nh.param("map_checker",map_checker,{false});
+    sub_map = nh.subscribe("/map",10,&AStarPath::map_callback,this);
+    pub_map = nh.advertise<nav_msgs::OccupancyGrid>("/map",1);
 
 }
 
 void AStarPath::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
 {
-    if(!map_checker)
+    if(map_checker) return;
+    else
     {
         the_map = *msg;
 
@@ -18,12 +21,13 @@ void AStarPath::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
         row = the_map.info.height;
         col = the_map.info.width;
         map_grid = std::vector<std::vector<int>>(row,std::vector<int>(col,0));
+        map_grid_copy = std::vector<std::vector<int>>(row,std::vector<int>(col,0));
 
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
-                map_grid[i][j] = the_map.data[j+i*j];
+                map_grid[i][j] = the_map.data[j+i*row];
             }
         }
 
@@ -35,28 +39,98 @@ void AStarPath::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
 
 void AStarPath::thick_wall()
 {
-    for(int i=0;i<row;i++)
+    if(!wall_checker)
     {
-        for(int j=0;j<col;j++)
-        {
-        }
-    }
+        //map_grid_copy = map_grid;
 
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                map_grid_copy[i][j] = map_grid[i][j];
+            }
+        }
+
+
+        for(int i=1;i<row-1;i++)
+        {
+            for(int j=1;j<col-1;j++)
+            {
+                if(map_grid[i][j]==100)
+                {
+                    map_grid_copy[i+1][j]==100;
+                    map_grid_copy[i][j+1]==100;
+                    map_grid_copy[i+1][j+1]==100;
+                }
+
+            }
+        }
+        //map_grid = map_grid_copy;
+
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                map_grid[i][j] = map_grid_copy[i][j];
+            }
+        }
+
+
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                the_map.data[j+i*row] = map_grid[i][j];
+            }
+        }
+
+
+        pub_map.publish(the_map);
+        wall_checker = true;
+    }
 
 }
 
 void AStarPath::set_goal()
 {
-
+    //goal = {{,},{,},{,},{,},{,},{,}};
+    //goal[0]:startpoint
+    //goal[5]:goalpoint
 }
 
-void AStarPath::make_heuristic()
+void AStarPath::make_heuristic(int next,int pre)
 {
+    dx = abs(goal[next].x-goal[pre].x);
+    dy = abs(goal[next].y-goal[pre].y);
+
+    heuristic = std::vector<std::vector<int>>(dx,std::vector<int>(dy,0));
+    for(int i=dx-1;i<=0;i--)
+    {
+        for(int j=dy-1;j<=0;j--)
+        {
+            heuristic[i][j] = abs(goal[next].x-i+1) + abs(goal[next].y-j+1);
+        }
+    }
 
 }
 
 void AStarPath::A_star()
 {
+
+    for(int i=1;i<5;i++)
+    {
+        make_heuristic(i,i-1);
+        reached = false;
+
+        while(!reached)
+        {
+
+            if(i=1) reached = true;//ここよくわかんなくなってる。
+
+        }
+
+
+    }
 
 }
 
@@ -68,7 +142,17 @@ void AStarPath::process()
         if(map_checker)
         {
             std::cout<<"ok1"<<std::endl;
+            thick_wall();
+            std::cout<<"ok2"<<std::endl;
+
         }
+        else
+        {
+            // std::cout<<"waaa"<<std::endl;
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
     }
 
 }
