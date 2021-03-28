5cf24faf34b471fa9b244bd43445687d10ff08e1
Hori3538 <yu.m.h.94612@gmail.com>
2021-03-29 01:51:46 +0900

    motion update書いた

--- include/Localizer/Localizer.h
+++ include/Localizer/Localizer.h
@@ -23,10 +23,15 @@ class Localizer
             public:
                 Particle(Localizer* localizer);
                 geometry_msgs::PoseStamped p_pose;
+                void set_p(double x, double y, double yaw, double x_sigma, double y_sigma, double yaw_sigma);
+                void p_move(double dx, double dy, double dyaw);
 
             private:
                 int p_pose_to_map_index(geometry_msgs::PoseStamped p_pose);
-                void set_p(double x, double y, double yaw, double x_sigma, double y_sigma, double yaw_sigma);
+                double create_yaw_from_msg(geometry_msgs::Quaternion q);
+                double substract_yawA_from_yawB(double yawA, double yawB);
+
+
 
                 Localizer* mcl;
                 ros::NodeHandle private_nh;
@@ -39,6 +44,9 @@ class Localizer
         Particle make_particle();
         double gaussian(double mu, double sigma);
         void create_p_pose_array_from_p_array(std::vector<Particle> p_array);
+        double substract_yawA_from_yawB(double yawA, double yawB);
+        double create_yaw_from_msg(geometry_msgs::Quaternion q);
+        void motion_update();
 
         int hz;
         int particle_number;
@@ -48,8 +56,10 @@ class Localizer
         double init_x_sigma;
         double init_y_sigma;
         double init_yaw_sigma;
+        double move_noise_ratio;
 
         bool map_get_ok = false;
+        bool odometry_get_ok = false;
 
         ros::NodeHandle private_nh;
         ros::NodeHandle nh;
@@ -62,7 +72,8 @@ class Localizer
 
         nav_msgs::OccupancyGrid map;
         sensor_msgs::LaserScan laser;
-        nav_msgs::Odometry odometry;
+        nav_msgs::Odometry current_odometry;
+        nav_msgs::Odometry previous_odometry;
         geometry_msgs::PoseStamped mcl_pose;
         geometry_msgs::PoseArray p_pose_array;
         std::vector<Particle> p_array;
--- src/Localizer.cpp
+++ src/Localizer.cpp
@@ -9,7 +9,9 @@ Localizer::Localizer():private_nh("~")
     private_nh.getParam("init_yaw", init_yaw);
     private_nh.getParam("init_x_sigma", init_x_sigma);
     private_nh.getParam("init_y_sigma", init_y_sigma);
-    private_nh.getParam("init_yaw_sigma", init_y_sigma);
+    private_nh.getParam("init_yaw_sigma", init_yaw_sigma);
+    private_nh.getParam("move_noise_ratio", move_noise_ratio);
+
 
     map_sub = nh.subscribe("/map", 100, &Localizer::map_callback, this);
     laser_sub = nh.subscribe("/scan", 100, &Localizer::laser_callback, this);
@@ -22,6 +24,7 @@ Localizer::Localizer():private_nh("~")
 void Localizer::laser_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
     laser = *msg;
+
 }
 void Localizer::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
 {
@@ -30,14 +33,20 @@ void Localizer::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
     ROS_INFO("map get");
     for(int i=0; i<particle_number; i++){
         Particle p = make_particle();
-        ROS_INFO("make particle");
     }
+    ROS_INFO("make particles");
     create_p_pose_array_from_p_array(p_array);
 }
 
 void Localizer::odometry_callback(const nav_msgs::Odometry::ConstPtr &msg)
 {
-    odometry = *msg;
+    if(map_get_ok){
+        previous_odometry = current_odometry;
+        current_odometry = *msg;
+        if(!odometry_get_ok){previous_odometry = current_odometry;}
+        motion_update();
+        odometry_get_ok = true;
+    }
 }
 
 Localizer::Particle Localizer::make_particle()
@@ -64,13 +73,25 @@ void Localizer::create_p_pose_array_from_p_array(std::vector<Particle> p_array)
     }
 }
 
+void Localizer::motion_update()
+{
+    double dx = current_odometry.pose.pose.position.x - previous_odometry.pose.pose.position.x;
+    double dy = current_odometry.pose.pose.position.y - previous_odometry.pose.pose.position.y;
+    double current_yaw = create_yaw_from_msg(current_odometry.pose.pose.orientation);
+    double previous_yaw = create_yaw_from_msg(previous_odometry.pose.pose.orientation);
+    double dyaw = substract_yawA_from_yawB(previous_yaw, current_yaw);
+    for(int i=0; i<p_array.size(); i++){
+        p_array.at(i).p_move(dx, dy, dyaw);
+    }
+}
+
 void Localizer::process()
 {
     ros::Rate rate(hz);
     while(ros::ok()){
-
+        create_p_pose_array_from_p_array(p_array);
         p_pose_array_pub.publish(p_pose_array);
-        ROS_INFO("publish p_pose_array");
+        //ROS_INFO("publish p_pose_array");
         ros::spinOnce();
         rate.sleep();
     }
@@ -100,6 +121,37 @@ void Localizer::Particle::set_p(double x, double y, double yaw, double x_sigma, 
         p_pose.pose.position.y = mcl->gaussian(y, y_sigma);
         quaternionTFToMsg(tf::createQuaternionFromYaw(mcl->gaussian(yaw,yaw_sigma)),p_pose.pose.orientation);
     }while(mcl->map.data[p_pose_to_map_index(p_pose)] != 0);
+}
+
+double Localizer::create_yaw_from_msg(geometry_msgs::Quaternion q)
+{
+    double roll, pitch, yaw;
+    tf::Quaternion quaternion(q.x, q.y, q.z, q.w);
+    tf::Matrix3x3(quaternion).getRPY(roll, pitch, yaw);
+    return yaw;
+}
+
+double Localizer::substract_yawA_from_yawB(double yawA, double yawB)
+{
+    double dyaw = yawB - yawA;
+    if(dyaw < -M_PI){dyaw += 2*M_PI;}
+    if(dyaw > M_PI){dyaw -= 2*M_PI;}
+
+    return dyaw;
+}
+
+void Localizer::Particle::p_move(double dx, double dy, double dyaw)
+{
+    //微妙な処理　要検討
+    double dist = sqrt(dx*dx + dy*dy);
+    dist += mcl->gaussian(0.0, dist * mcl->move_noise_ratio);
+    double yaw = mcl->create_yaw_from_msg(p_pose.pose.orientation);
+    yaw += mcl->gaussian(dyaw, dyaw * mcl->move_noise_ratio);
+    if(yaw > M_PI){yaw -= 2*M_PI;}
+    if(yaw < -M_PI){yaw += 2*M_PI;}
+    p_pose.pose.position.x += dist * cos(yaw);
+    p_pose.pose.position.y += dist * sin(yaw);
+    quaternionTFToMsg(tf::createQuaternionFromYaw(yaw), p_pose.pose.orientation);
 }
 
 int main(int argc, char **argv)
