d3d71316c223c570da3ce99a51fb5c8aa755e43d
MarikaTakasago <marikanboumarch@yahoo.co.jp>
2021-04-10 17:53:54 +0900

    1周してるけど無駄な線が…

--- include/Global_path_planner/Global_path_planner.h
+++ include/Global_path_planner/Global_path_planner.h
@@ -10,16 +10,20 @@ struct open
 {
     int f;
     int g;
-    int h;
+    int pre_x;
+    int pre_y;
+};
+
+struct twod
+{
     int x;
     int y;
 };
 
-struct twod
-
+struct ftwod
 {
-    int x;
-    int y;
+    float x;
+    float y;
 };
 
 
@@ -35,7 +39,7 @@ class AStarPath
         void map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
         void thick_wall(); //壁を増やして本当の壁にぶつからないようにする。
         void set_goal(); //goalを決める
-        void make_heuristic(int,int); //heuristic関数をつくる
+        void make_heuristic(int); //heuristic関数をつくる
         void A_star(); //A*プロセス
 
 
@@ -48,17 +52,44 @@ class AStarPath
         std::vector<std::vector<int>> map_grid_copy; //2Dmapcopy
 
         std::vector<std::vector<int>> heuristic; //heuristic関数
-        int dx;
-        int dy;
+        //int dx;
+        //int dy;
+
         std::vector<twod> goal; //set_goal
 
-        std::vector<std::vector<int>> closed;
-        std::vector<std::vector<int>> open;
+
+
+        std::vector<std::vector<open>> close_list;
+        std::vector<std::vector<open>> open_list;
+        open next;
+
+        float res;
+
+        int gx;//goal
+        int gy;
+        int x2;
+        int y2;
+        int x;//now
+        int y;
+        int old_x;
+        int old_y;
+
+        std::vector<twod> delta;
+
+        int g;
+        int h;
+        int f[4];
+
+        ftwod origin;//-100？？
+        twod mom;//親
+        twod dad;//親のリマインダー
 
         bool map_checker;//map貰ったかどうか✓
         bool wall_checker;//壁増やした？
         bool reached;//goalについたか
-
+        bool heu_checker;//heuristic関数つくった？
+        bool startpoint;//oldをたどったときの開始地点に戻ってきた？
+        bool path_checker;//pathできた？？
 
         //member
         ros::NodeHandle nh;
@@ -68,6 +99,7 @@ class AStarPath
         ros::Publisher pub_path;
         nav_msgs::OccupancyGrid the_map;//map格納
         nav_msgs::Path global_path;//Local_path_plannerにおくる
+        nav_msgs::Path mini_path;//仮のゴール間のpath
         geometry_msgs::PoseStamped mcl_pose;//Localizerからもらう
 
 
--- src/Global_path_planner.cpp
+++ src/Global_path_planner.cpp
@@ -6,6 +6,8 @@ AStarPath::AStarPath():private_nh("")
     private_nh.param("hz",hz,{10});
     private_nh.param("map_checker",map_checker,{false});
     private_nh.param("wall_checker",wall_checker,{false});
+    private_nh.param("heu_checker",heu_checker,{false});
+    private_nh.param("path_checker",path_checker,{false});
     sub_map = nh.subscribe("/map",10,&AStarPath::map_callback,this);
     pub_map = nh.advertise<nav_msgs::OccupancyGrid>("/map",1);
     pub_path = nh.advertise<nav_msgs::Path>("/path",1);
@@ -33,6 +35,10 @@ void AStarPath::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
             }
         }
 
+        origin.x = the_map.info.origin.position.x;
+        origin.y = the_map.info.origin.position.y;
+        std::cout<<"origin:"<<origin.x<<","<<origin.y<<std::endl;
+
         map_checker = true;
         std::cout<<"map_grid"<<std::endl;
     }
@@ -49,12 +55,12 @@ void AStarPath::thick_wall()
         {
             for(int j=0;j<col;j++)
             {
+                if((i>2282 && i<2287 && j>1547 && j<1893) || (i>1997 && i<2282 && j<1547 && j>1553) || (i>1997 && i<2003 && j>1547 && j<2213) || (i>2003 && i<2287 && j>2207 && j<2213))
+                    if(map_grid[i][j]==100)
+                    {
+                        map_grid[i][j] = 0;
+                    }
                 map_grid_copy[i][j] = map_grid[i][j];
-             /*   if(map_grid[i][j]==100)
-                {
-                    std::cout<<map_grid_copy[i][j];
-                    std::cout<<",";
-                }*/
             }
            // std::cout<<std::endl;
         }
@@ -66,6 +72,24 @@ void AStarPath::thick_wall()
         {
             for(int j=5;j<col-5;j++)
             {
+                /*if(i>2282 && i<2287 && j>1547 && j<1893)
+                {
+                    if(map_grid_copy[i][j] == 100)
+                    {
+                        map_grid_copy[i][j] = 0;
+                        map_grid[i][j] = 0;
+                    }
+                }
+
+                if(i>2282 && i<2287 && j>1547 && j<1893)
+                {
+                    if(map_grid[i][j] == 100)
+                    {
+                        map_grid_copy[i][j] = 0;
+                        map_grid[i][j] = 0;
+                    }
+                }*/
+
                 if(map_grid_copy[i][j]==100)
                 {
                     map_grid[i+1][j]=100;
@@ -76,25 +100,23 @@ void AStarPath::thick_wall()
                     map_grid[i+2][j]=100;
                     map_grid[i+2][j+1]=100;
                     map_grid[i+2][j+2]=100;
-                    map_grid[i+3][j]=100;
-                    map_grid[i+3][j+2]=100;
-                    map_grid[i+3][j+3]=100;
-                    map_grid[i+4][j]=100;
-                    map_grid[i+4][j+2]=100;
-                    map_grid[i+4][j+3]=100;
-                    map_grid[i+4][j+4]=100;
 
                     map_grid[i][j-1]=100;
                     map_grid[i][j-2]=100;
-                    map_grid[i][j-3]=100;
+                    map_grid[i-1][j]=100;
+                    map_grid[i-1][j-1]=100;
+                    map_grid[i-1][j-2]=100;
+                    map_grid[i-2][j]=100;
+                    map_grid[i-2][j-1]=100;
+                    map_grid[i-2][j-2]=100;
 
-                    std::cout<<"wo!"<<std::endl;
-                    count += 1;
+                    //std::cout<<"wo!"<<std::endl;
+                //    count += 1;
                 }
+
 
             }
         }
-        //map_grid = map_grid_copy;
 
         /*for(int i=0;i<row;i++)
         {
@@ -103,6 +125,39 @@ void AStarPath::thick_wall()
                 map_grid[i][j] = map_grid_copy[i][j];
             }
         }*/
+
+        /*for(int i=0;i<100;i++)
+        {
+            int j = 2285;
+            map_grid[j][1895+i] = 80;
+            map_grid[j][1896+i] = 60;
+            map_grid[j+1][1895+i] = 60;
+            map_grid[j+1][1896+i] = 60;
+            map_grid[j][1550+i] = 80;
+            map_grid[j][1551+i] = 60;
+            map_grid[j+1][1550+i] = 60;
+            map_grid[j+1][1551+i] = 60;
+
+        }*/
+
+        /*map_grid[2000][1550] = 80;
+        map_grid[2001][1550] = 80;
+        map_grid[2000][2210] = 80;
+        map_grid[2001][2210] = 80;
+        map_grid[2285][1550] = 80;
+        map_grid[2286][1550] = 80;
+        map_grid[2285][2210] = 80;
+        map_grid[2286][2210] = 80;
+        map_grid[2285][1890] = 80;
+        map_grid[2286][1891] = 80;*/
+
+        res = the_map.info.resolution;
+        std::cout<<res<<std::endl;
+
+        std::cout<<map_grid[2000][1550]<<std::endl;
+        std::cout<<map_grid[1999][1666]<<std::endl;
+        std::cout<<map_grid[2285][1642]<<std::endl;
+        std::cout<<map_grid[2285][1641]<<std::endl;
 
 
         for(int i=0;i<row;i++)
@@ -114,10 +169,7 @@ void AStarPath::thick_wall()
         }
 
         std::cout<<"wall"<<std::endl;
-        std::cout<<count<<std::endl;
-
-
-
+        //std::cout<<count<<std::endl;
 
         pub_map.publish(the_map);
         wall_checker = true;
@@ -127,45 +179,223 @@ void AStarPath::thick_wall()
 
 void AStarPath::set_goal()
 {
-    //goal = {{,},{,},{,},{,},{,},{,}};
+    goal = {{2285,1890},{2285,1550},{2000,1550},{2000,2210},{2285,2210},{2285,1890}};
     //goal[0]:startpoint
     //goal[5]:goalpoint
+    std::cout<<"set_goal"<<std::endl;
 }
 
-void AStarPath::make_heuristic(int next,int pre)
+void AStarPath::make_heuristic(int next)
 {
-    dx = abs(goal[next].x-goal[pre].x);
-    dy = abs(goal[next].y-goal[pre].y);
+    //dx = abs(goal[next].x-goal[pre].x)+1;
+    //dy = abs(goal[next].y-goal[pre].y)+1;
 
-    heuristic = std::vector<std::vector<int>>(dx,std::vector<int>(dy,0));
-    for(int i=dx-1;i<=0;i--)
+    heuristic = std::vector<std::vector<int>>(row,std::vector<int>(col,0));
+    std::cout<<"startheu"<<std::endl;
+    //std::cout<<dx<<std::endl;
+    //std::cout<<dy<<std::endl;
+    for(int i=0;i<row;i++)
     {
-        for(int j=dy-1;j<=0;j--)
+        for(int j=0;j<col;j++)
         {
-            heuristic[i][j] = abs(goal[next].x-i+1) + abs(goal[next].y-j+1);
+            heuristic[i][j] = abs(goal[next].x - i) + abs(goal[next].y - j);
+            //std::cout<<heuristic[i][j];
+            //std::cout<<",";
         }
+        //std::cout<<std::endl;
     }
+    //std::cout<<"--------------------"<<std::endl;
 
 }
 
 void AStarPath::A_star()
 {
+    std::cout<<"A*start"<<std::endl;
 
-    for(int i=1;i<5;i++)
+    delta = {{0,-1},{-1,0},{0,1},{1,0}};//R,Up,L,Down
+    open_list = std::vector<std::vector<open>>(row,std::vector<open>(col));
+
+    /*origin.x = the_map.info.origin.position.x;
+    origin.y = the_map.info.origin.position.y;
+    std::cout<<"origin:"<<origin.x<<","<<origin.y<<std::endl;
+*/
+
+    for(int r=0;r<row;r++)
     {
-        make_heuristic(i,i-1);
+        for(int c=0;c<col;c++)
+        {
+            open_list[r][c].f = 0;
+            open_list[r][c].g = 0;
+            open_list[r][c].pre_x = 0;
+            open_list[r][c].pre_y = 0;
+        }
+    }
+    std::cout<<"setopen_list"<<std::endl;
+
+    for(int i=0;i<5;i++)
+    {
+        make_heuristic(i+1);
         reached = false;
+
+        close_list = std::vector<std::vector<open>>(row,std::vector<open>(col));
+        for(int r=0;r<row;r++)
+        {
+            for(int c=0;c<col;c++)
+            {
+                close_list[r][c].f = 0;
+                close_list[r][c].g = 0;
+                close_list[r][c].pre_x = 0;
+                close_list[r][c].pre_y = 0;
+                open_list[r][c].f = 0;
+                open_list[r][c].g = 0;
+                open_list[r][c].pre_x = 0;
+                open_list[r][c].pre_y = 0;
+            }
+        }
+
+        std::cout<<"setclose_list"<<std::endl;
+
+        int gx = goal[i+1].x;
+        int gy = goal[i+1].y;
+        int x = goal[i].x;
+        int y = goal[i].y;
+        int g = 0;
+
+
+        std::cout<<"start_searching"<<std::endl;
 
         while(!reached)
         {
+            if(x == gx && y == gy)
+            {
+                std::cout<<"reached"<<std::endl;
+                reached = true;
+            }
+            else
+            {
+                g += 1;
+                for(int j=0;j<4;j++)
+                {
+                    //std::cout<<j;
+                    x2 = x + delta[j].x;
+                    y2 = y + delta[j].y;
 
-            if(i=1) reached = true;//ここよくわかんなくなってる。
+                    if(x2 >= 0 && x2<row && y2 >= 0 && y2<col)
+                    {
+                        if(close_list[x2][y2].g == 0 && map_grid[x2][y2] != 100)
+                        {
+                            f[j] = g + heuristic[x2][y2];
+                            open_list[x2][y2].g = g;
+                            open_list[x2][y2].f = f[j];
+                            open_list[x2][y2].pre_x = x;
+                            open_list[x2][y2].pre_y = y;
+                            close_list[x2][y2].pre_x = x;
+                            close_list[x2][y2].pre_y = y;
+                            close_list[x2][y2].g = 1;
+                        }
+                        else
+                        {
+                            //close_list[x2][y2].g = 1;
+                            f[j] = 4000;
+                        }
+                    }
+                }
+
+                int fmin = f[0];
+                int kmin = 0;
+                for(int k=0;k<4;k++)//f値が一番小さいのいつだった？
+                {
+                    if(f[k]<fmin)
+                    {
+                        fmin = f[k];
+                        kmin = k;
+                    }
+                }
+
+                if(fmin==4000)
+                {
+                    close_list[old_x][old_y].g = 0;
+                    for(int k=0;k<4;k++)
+                    {
+                        close_list[old_x + delta[k].x][old_y + delta[k].y].g = 0;
+                        close_list[old_x + delta[k].x][old_y + delta[k].y].pre_x = 0;
+                        close_list[old_x + delta[k].x][old_y + delta[k].y].pre_y = 0;
+                    }
+                    close_list[x][y].g = 1;
+            //        map_grid[x][y] = 100;
+                    x = old_x;
+                    y = old_y;
+
+                }
+
+                if(fmin!=4000)
+                {
+                    old_x = x;
+                    old_y = y;
+            /*std::cout<<close_list[x][y].pre_x;
+            std::cout<<",";
+            std::cout<<close_list[x][y].pre_y<<std::endl;*/
+                    x = x + delta[kmin].x;
+                    y = y + delta[kmin].y;
+                    std::cout<<close_list[x][y].pre_x;
+                    std::cout<<",";
+                    std::cout<<close_list[x][y].pre_y;
+                    std::cout<<",";
+                    std::cout<<x;
+                    std::cout<<",";
+                    std::cout<<y;
+                    std::cout<<",";
+                    std::cout<<open_list[x][y].f<<std::endl;
+                }
+            }
+
 
         }
+        startpoint = false;
+        //mini_path
+        mini_path.header.frame_id = "map";
+        geometry_msgs::PoseStamped point;
+        point.pose.position.y = (x-row/2)*res;// + origin.x;
+        point.pose.position.x = (y-col/2)*res;// + origin.y;
+        mini_path.poses.push_back(point);
 
+        mom.x = close_list[x][y].pre_x;
+        mom.y = close_list[x][y].pre_y;
+        std::cout<<"momdad"<<std::endl;
+        std::cout<<mom.x<<std::endl;
+        std::cout<<mom.y<<std::endl;
 
+        while(!startpoint)
+        {
+            if(mom.x == goal[i].x && mom.y == goal[i].y)
+            {
+                point.pose.position.y = (mom.x-row/2)*res;
+                point.pose.position.x = (mom.y-col/2)*res;
+                mini_path.poses.push_back(point);
+                startpoint = true;
+            }
+
+            else
+            {
+                    point.pose.position.y = (mom.x-row/2)*res;// + origin.x;
+                    point.pose.position.x = (mom.y-col/2)*res;// + origin.y;
+                    mini_path.poses.push_back(point);
+
+                    dad.x = mom.x;
+                    dad.y = mom.y;
+                    mom.x = close_list[dad.x][dad.y].pre_x;
+                    mom.y = close_list[dad.x][dad.y].pre_y;
+
+                    //if(mom.y != 0) std::cout<<mom.y<<std::endl;
+            }
+        }
+
+        std::cout<<"============="<<std::endl;
+        std::reverse(mini_path.poses.begin(),mini_path.poses.end());
+        global_path.header.frame_id = "map";
+        global_path.poses.insert(global_path.poses.end(),mini_path.poses.begin(),mini_path.poses.end());
     }
-
+    std::cout<<"--------------------"<<std::endl;
 }
 
 void AStarPath::process()
@@ -173,18 +403,27 @@ void AStarPath::process()
     ros::Rate loop_rate(hz);
     while(ros::ok())
     {
-        if(map_checker)
+        if(map_checker && !path_checker)
         {
             //std::cout<<"ok1"<<std::endl;
             thick_wall();
-            //std::cout<<"ok2"<<std::endl;
+            set_goal();
+            /*if(!heu_checker)
+            {
+                for(int i=0;i<5;i++)
+                {
+                    make_heuristic(i+1,i);
+                }
+                heu_checker = true;
+            }*/
+            A_star();
+            std::cout<<"ok2"<<std::endl;
+            pub_path.publish(global_path);
+            std::cout<<"fin"<<std::endl;
+            path_checker = true;
 
         }
-        else
-        {
-            // std::cout<<"waaa"<<std::endl;
-        }
-
+        std::cout<<"FIN"<<std::endl;
         ros::spinOnce();
         loop_rate.sleep();
     }
